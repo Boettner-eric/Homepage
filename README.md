a30b35293dcc065dce68864130b8d95d3bb8290f
Eric Boettner <ericboettner@Erics-MacBook-Pro.local>
2018-03-24 19:34:40 -0700

    Added Scripting

--- README.md
+++ README.md
@@ -1,41 +1,75 @@
-Homepage
+# Homepage
 
-[Hosted on Gitub.io](https://boettner-eric.github.io/Homepage/)
+## [Hosted on Gitub.io](https://boettner-eric.github.io/Homepage/)
 ![Template.html (github public version)](template.png)
 
-This project is an attempt at a better desktop and mobile launch page.  It has keyboard shortcuts, nested folders, cool design and is easy to edit.
+This project is an attempt at a better desktop and mobile launch page.  It has keyboard shortcuts, nested folders, cool design and is super easy to edit
 
-Features:
-- Nightmode (small moon in bottom left)
-  to edit nightmode/base colors open keys.js and edit line 27/29
-  <
-  27 document.documentElement.style.setProperty('--background', '#CACFD2');
-  28 document.documentElement.style.setProperty('--background-alt', '#CACFD2');
-  29 document.documentElement.style.setProperty('--base-txt', '#3A529B');
-  >
-- Fully customizable tiles and nested folders
+# Features:
+* ### Scripted page creation
+ The script.py file generates html files with minimal user input
+```
+    git $ python3 script.py
+    mode : s g (single/generate) -  g
+    enter filename : sample
+    ---------------------
+    tile : 1
+    link : https://Github.com
+    name : Github
+    subtittle : open source
+    src : gh
+    ---------------------
+    tile : 2
+    link : https://stackoverflow.com
+    name : Stack Overflow
+    subtittle : Dev help
+    src : st
+    ---------------------
+    tile : 3
+    link : https://Google
+    name : Google
+    subtittle : search
+    src : go
+    ---------------------
+    File Created
+```
+ ![sample.html](example.png)
+
+* ### Nightmode
+(small moon icon in bottom left)
+  to edit nightmode/base colors open keys.js and edit lines 27-29
+  ```javascript
+  document.documentElement.style.setProperty('--background', '#CACFD2');
+  document.documentElement.style.setProperty('--background-alt', '#CACFD2');
+  document.documentElement.style.setProperty('--base-txt', '#3A529B');
+```
+![nightmode](nightmode.png)
+* ### Fully customizable tiles and nested folders
    To edit a tile open template.html.  The main block of code should look like this:
+```html
+<a href="insert link" id="1" class="lBox">
+	<div class="button">
+	    <img src="src/ba.png" style='max-width: 50px'>
+	</div>
+	<h3>Back</h3>
+    <p>To the Future</p>
+</a>
+```
 
-          <a href="insert link" id="1" class="lBox">
-		<div class="button">
-	       	    <img src="src/ba.png" style='max-width: 40px'>
-		</div>
-		<h3>Back</h3>
-                <p>To the Future</p>
-            </a>
-
-  h3 displays the website tittle, p the subtittle and "button" contains the image, just edit these and you have a new tile
+  - h3 contains the website tittle,
+  - p contains a  subtittle
+  - "button" contains the image
+  -
 
   To add a folder just copy the template.html file and rename it.  In the original add a tile for your folder and in the new html file just change the url of back to "template.html" or the url of the source page.  
 
-- Responsive design
+* ### Responsive design
 ![Responsive layout](responsive.png)
 
-- An example subfolder
-
+* ### An example subfolder
 ![My setup w/ folders and icons](folder.png)
 
 
-* This is a fork of - https://github.com/Capuno/Decaux - My version is very different but started from this open source project
+* This is a fork of - https://github.com/Capuno/Decaux - My version is very different but started from this code base
 
-- All icons are from https://icons8.com [color theme]
+* All icons are from the - https://icons8.com color theme
---
+++ script.py
@@ -0,0 +1,93 @@
+# Eric Boettner 3/24/18
+# script.py
+# generates a formatted html template file for boettner-eric/homepage (github)
+
+# See Readme.md for more info and detailed instructions
+
+x = input("mode : s g (single/generate)  -  ")
+# s - generate one tile - for edits/corrections (just copy and paste into html file)
+# g - creates a fully formatted html file with 12 tiles
+
+if x == "g":
+    filename = input("enter filename : ")
+    print("---------------------")
+    num = 1
+    f = open(filename + ".html", "w")
+    f.write("<!DOCTYPE html>")
+    f.write("<html>\n")
+    f.write("<head>\n")
+    f.write("    <title>"+ filename +"</title>\n")
+    f.write("    <meta charset=\"utf-8\">\n")
+    f.write("    <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">\n")
+    f.write("</head>\n")
+    f.write("<body>\n")
+    f.write("    <div id=\"grid\">\n")
+    f.write("        <table>\n")
+    f.write("            <tr>\n")
+    link = []
+    name = []
+    subtittle = []
+    src = []
+    for i in range(13):
+        link.append("")
+        name.append("")
+        subtittle.append("")
+        src.append("")
+    num = 1
+    while num < 13:
+        print("tile : " + str(num))
+        link[num] = input("link : ")
+        name[num] = input("name : ")
+        subtittle[num] = input("subtittle : ")
+        src[num] = input("src : ")
+        print("---------------------")
+        num += 1
+    num = 1
+    # Adds All 12 tiles
+    while num < 13:
+        f.write("                <td>\n")
+        f.write("                    <a href= \"" + link[num] + "\" id="+ str(num) + " class=\"lBox\">\n")
+        f.write("                        <div class=\"button\">\n")
+        f.write("                            <img src=\"src/" + src[num] + ".png\"" + " style=\'max-width: 50px\'>\n")
+        f.write("                        </div>\n")
+        f.write("                        <h3>" + name[num] + "</h3>\n")
+        f.write("                        <p>" + subtittle[num] + "</p>\n")
+        f.write("                    </a>\n")
+        f.write("                </td>\n")
+        if num % 4 == 0:
+            f.write("            <tr>\n")
+            f.write("            </tr>\n")
+        num += 1
+    # Adds footer
+    f.write("            </tr>\n")
+    f.write("        </table>\n")
+    f.write("    </div>\n")
+    f.write("<footer>\n")
+    f.write("    <form action=\"https://google.com/search\" method=\"get\">\n")
+    f.write("         <input type=\"text\" name=\"q\" placeholder=\"[search]\" id=\"search\" autocomplete=\"off\">\n")
+    f.write("    </form>\n")
+    f.write("    <button id=\"night\", class=\"night\">\n")
+    f.write("    	<img src=\"src/mn.png\" style=\'max-width: 30px\'>\n")
+    f.write("    </button>\n")
+    f.write("</footer>\n")
+    f.write("</body>\n")
+    f.write("<script type=\"text/javascript\" src=\"keys.js\"></script>\"\n")
+    f.write("</html>\n")
+    print("File Created")
+
+
+else:
+    num = input("num : ")
+    link = input("link : ")
+    name = input("name : ")
+    subtittle = input("subtittle : ")
+    src = input("src : ")
+    print("----------------------------------------------\n")
+    print("<a href= \"" + link + "\" id="+ num + " class=\"lBox\">")
+    print("    <div class=\"button\">")
+    print("        <img src=\"src/" + src + ".png\"" + " style=\'max-width: 50px\'>")
+    print("    </div>")
+    print("    <h3>" + name + "</h3>")
+    print("    <p>" + subtittle + "</p>")
+    print("</a>")
+    print("\n----------------------------------------------\n")
